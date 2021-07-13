6102c32237c3afa757e1f8374424e9722e7f8159
felixyoma4u <you@example.com>
2021-07-13 12:19:37 +0100

    Initial commit

---
+++ app/src/main/java/com/emoyoma/todoapp/Profile_Activity.kt
@@ -0,0 +1,11 @@
+package com.emoyoma.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
